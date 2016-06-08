a78a42335fecb391704dd19e2d893957acceec13
Wenjie Guo <wguo@redhat.com>
2016-06-08 15:07:11 +0800

    update singleton way

--- src/main/java/com/example/bpmsremote/client2/BpmsClientUtil.java
+++ src/main/java/com/example/bpmsremote/client2/BpmsClientUtil.java
@@ -3,15 +3,13 @@ package com.example.bpmsremote.client2;
 
 public class BpmsClientUtil {
 
-    static final BpmsClient client;
+    private static BpmsClient client = null;
+    private static final String url = "http://{host}:{port}/business-central/rest";
 
-    static {
-        String url = "https://maitai-bpms-dt.host.qe.eng.pek2.redhat.com/business-central/rest";
-        //String url = "http://10.66.136.79:8080/business-central/rest";
-        client = BpmsClientImpl.create(url);
-    }
-
-    public static BpmsClient setUp() {
+    public synchronized static BpmsClient getBpmsClient() {
+        if (client == null){
+            client = BpmsClientImpl.create(url);
+        }
         return client;
     }
 
